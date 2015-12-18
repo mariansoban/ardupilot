d287eb341fcf0ca5f47ba18ba53a09d2195bde73
Marian Soban <marian.soban@gmail.com>
2015-12-18 12:29:22 +0100

    release notes, version update

--- ArduCopter/Copter.h
+++ ArduCopter/Copter.h
@@ -1,6 +1,6 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-#define THISFIRMWARE "APM:Copter V3.3.2-phl-sobi"
+#define THISFIRMWARE "APM:Copter V3.3.2-phl-sobi-8fm"
 #define FIRMWARE_VERSION 3,3,2,FIRMWARE_VERSION_TYPE_DEV
 
 /*
