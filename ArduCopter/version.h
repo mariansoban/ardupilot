0834a353f396a1e7e418f782d9d23b59b0f714f3
Marian Soban <marian.soban@gmail.com>
2016-12-13 23:14:52 +0100

    version update, release notes

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "APM:Copter V3.4.3-phl-sobi-8fm"
+#define THISFIRMWARE "APM:Copter V3.4.3-phl-sobi-8fm-o"
 #define FIRMWARE_VERSION 3,4,3,FIRMWARE_VERSION_TYPE_DEV
 
 #ifndef GIT_VERSION
