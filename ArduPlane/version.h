e739dffd17aa78fa7d39b94ee3e77bf0468f8b03
Marian Soban <marian.soban@gmail.com>
2020-04-23 21:25:26 +0200

    version update

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.0.9"
+#define THISFIRMWARE "ArduPlane V4.0.9-sobi-frsky"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,9,FIRMWARE_VERSION_TYPE_OFFICIAL
@@ -14,4 +14,4 @@
 #define FW_MAJOR 4
 #define FW_MINOR 0
 #define FW_PATCH 9
-#define FW_TYPE FIRMWARE_VERSION_TYPE_OFFICIAL
+#define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
